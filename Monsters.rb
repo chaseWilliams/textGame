b903a979eb8d20296ad426c2be7822b57eb2cb94
chaseWilliams <bornofawesomeness@gmail.com>
2015-12-15 21:12:13 -0500

    Almost done with multimonster compatibility; just potion at 0 turn skip problem

--- Monsters.rb
+++ Monsters.rb
@@ -7,39 +7,49 @@ class Monsters #kind of redundant right now, will add on
   @name
   @isDead
   @monsterHitMessage
-  
+
   def update
     @isDead = (@health <= 0)
     puts "#{@name} health at: #{@health}"
   end
-  
+
   def phyAttack
     $playerHealth -= 5
   end
-  
+
   def hitMessage
     return @monsterHitMessage + "#{$playerHealth}"
   end
-  
-  def health?
+
+  def health
     return @health
   end
-  
-  def monsterDead?
+
+  def monsterDead
     return @isDead
   end
-  
+
   def hit(value)
     @health = @health - value
   end
 end
 
-class Zombie < Monsters
+class Rogue < Monsters
   def initialize
     @health = 20
     @attack = 2
-    @monsterHitMessage = "You got bitten! Health now at "
+    @monsterHitMessage = "You got punched! Health now at "
     @name = 'Zombie'
+    @isDead = false
+  end
+end
+
+class Vampire < Monsters
+  def initialize
+    @health = 100
+    @attack = 7
+    @monsterHitMessage = 'The vampire bit you. Health now at '
+    @name = 'Vampire'
     @isDead = false
   end
 end
--- controller.rb
+++ controller.rb
@@ -15,30 +15,29 @@ class Controller
       #arguments are quantity, value
       potion: [1, 50]
     }
-    @itemCount = "#{$playerItems[:potion][0]} potions"
+    @itemCount = $playerItems[:potion][0]
     puts "Hello, welcome to RPG Legends, by Chase Williams"
     puts "Please enter a name"
     $playerName = gets.chomp
     puts "Lets do a practice fight, #{$playerName}\nPlease enter F for fight, or I to look at Items."
-    self.fight('Zombie')
-    puts "Congrats! Finished with #{$playerHealth} out of #{$playerMaxHealth}\nE n d  o f  S e q u e n c e"
+    self.fight('Rogue')
+    puts "Congrats! Finished with #{$playerHealth} out of #{$playerMaxHealth}\nLet's fight again!"
+    self.fight('Vampire')
   end
 
   def fight(monsterToFight) #needs to be developed so that it can instantiate and initiate fight
-    #with any monster, depending on parameter given. Right now only does 1 zombie
+    #with any monster, depending on parameter given.
     case monsterToFight
-    when "Zombie"
-      m = Zombie.new
+    when "Rogue"
+      m = ::Rogue.new
     when "Vampire"
-      puts "not ready yet"
-      return nil
+      m = ::Vampire.new
     else
       puts "error 503"
-      return nil
     end
-    
+
     puts "#{$playerName}, your health is at #{$playerHealth} / #{$playerMaxHealth}"
-    while (!m.monsterDead?)
+    while (!m.monsterDead)
       puts "Fight!\n"
       @input = gets.chomp
       while (@input != "F" && @input != "I")
@@ -47,40 +46,53 @@ class Controller
     end
     turn_over = false
     replay = false
-    while turn_over == false
-      if replay
-        puts "Enter an Action"
-        @input = gets.chomp
-      end    #checks to see if loop is running again, in that case
+
+    while !turn_over
+      if $playerHealth > 0
+        if replay
+          puts "Enter an Action"
+          @input = gets.chomp
+        end    #checks to see if loop is running again, in that case
         #new input is needed
-      if @input != 'F' && @input != 'I' then puts 'Invalid' end    #gatekeeper
-      if (@input == "F")
-        m.hit($playerAttack)
-        turn_over = true
-      elsif @input == 'I'
-        puts "You have #{@itemCount}\nEnter 'Potion' to use. Otherwise, enter anything"
-        @input = gets.chomp
-        if @input == 'Potion'
-          self.healPlayer($playerItems[:potion][1])
-          $playerItems[:potion[1]] = 0
-          turn_over = true
-          $playerItems[:potion][0] -= 1
-          self.itemUpdate
+        if @input != 'F' && @input != 'I' then puts 'Invalid' end    #gatekeeper
+          if (@input == "F")
+            m.hit($playerAttack)
+            turn_over = true
+          elsif @input == 'I'
+            puts "You have #{@itemCount} potions.\nEnter 'Potion' to use. Otherwise, enter anything"
+            @input = gets.chomp
+            if @input == 'Potion'
+              if @itemCount > 0
+                self.healPlayer($playerItems[:potion][1])
+                $playerItems[:potion[1]] = 0
+                turn_over = true
+                $playerItems[:potion][0] -= 1
+                self.itemUpdate
+              else
+                puts 'No more potions!'
+                turn_over = false
+                replay = true
+              end
+            else
+              replay = true
+            end
+          end
+
+        if (m.health > 0)
+          m.phyAttack
+          puts m.hitMessage
         else
-          replay = true
-        end #a;sdlghasd;glkhasd;lhk
-      end
-    end
-      if (m.health? > 0)
-        m.phyAttack
-        puts m.hitMessage
+          puts "You beat it!"
+        end
+        m.update
       else
-        puts "You beat it!"
+        puts 'Player dead, game over'
+        return nil
       end
-      m.update
     end
   end
-  
+end
+
   #player methods
   def healPlayer(value)
     if value + $playerHealth <= $playerMaxHealth
@@ -89,7 +101,7 @@ class Controller
       $playerHealth = $playerMaxHealth
     end
   end
-  
+
   def itemUpdate
     @itemCount = $playerItems[:potion][0]
   end
